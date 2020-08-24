420dbbb574fa6d96419cc9ec9945ec9d51e4f49d
Cristopher-ubuntu <cristopher.sotog@utem.cl>
2020-08-24 10:50:43 -0400

    Aprendiendo sobre el tipo VAR

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-console.log("Enviando a la consola");
-console.log('hasta aquí todo bien');
-console.log(2+2); /*Con console.log() se puede enviar todo tipo de valores a la consola.*/
+// var let const 
+
+var nombre = 'Juan'; //Definición de una variable nombre de tipo VAR.
+
+nombre = 'John'; //Sin necesidad de anteponer VAR (sin volverla a declarar) se puede cambiar el contenido de la variable de tipo VAR
+
+console.log(nombre); //Muestra la variable nombre por la consola del navegador
+
+var nombre2; //nombre2 de tipo VAR no se encuentra definida.
+
+console.log(nombre2); //En la consola del navegador saldrá como undefined
+
+var v1 = 0, v2 = 1, v3 = 2; // Se definen 3 variables de tipo VAR en una misma línea.
+
+console.log(v1); //Muestra v1
+console.log(v2); //Muestra v2
+console.log(v3); //Muestra v3
