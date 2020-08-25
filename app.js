d2179a26713b79fceeb6218d6bba4d9f812b86b0
Cristopher-ubuntu <cristopher.sotog@utem.cl>
2020-08-25 19:52:17 -0400

    Aprendiendo sobre el tipo CONST

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 // var let const 
 
-let variable1 = 'Libro'; //Se crea la variable1 de tipo let que contiene a Libro
+const producto = 'Libro'; //Se define una variable producto de tipo const
 
-console.log(variable1); //Se muestra por la consola del navegador la variable1
+console.log(producto); // Se muestra por consola del navegador la variable producto
 
-variable1 = 'Libro 1'; //Se cambia el valor de la variable1 a Libro 1
-
-console.log(variable1); //Se muestra por la consola del navegador la variable1
-
-let variable1 = 'Libro 2' //Tira error debido a que al ser de tipo let no se puede volver a declarar
-
-console.log(variable1); //Al intentar mostrar la variable1 sale un error en la consola del navegador. Cabe mencionar que debido al error que causa la redefinición de la variable1, la consola no muestra nada. 
+producto = 'Libro2' // La consola tira muestra el siguiente error: Uncaught TypeError: Assignment to constant variable.
